5afe996d40ddf893b436a49aca265d5cc5a65857
minhtien1710 <minhtien17102006@gmail.com>
2026-01-21 20:22:43 +0700

    Update sinhvien.cpp

--- sinhvien.cpp
+++ sinhvien.cpp
@@ -1,46 +1,107 @@
-#ifndef SINHVIEN_H
-#define SINHVIEN_H
+#include "sinhVien.h"
 
-#include <string>
-#include <vector>
-#include <iostream>
-#include <fstream>
-#include "doituong.h"
-using namespace std;
+SinhVien::SinhVien() {}
 
-class SinhVien: public DoiTuong {
-private:
-    string maSV;
-    string tenSV;
-    string lop;
-    string sdt;
-    vector<string> sachDangMuon;
+SinhVien::SinhVien(string ma, string ten, string l, string sd) 
+    : maSV(ma), tenSV(ten), lop(l), sdt(sd) {}
 
-public:
-    SinhVien();
-    SinhVien(string ma, string ten, string l, string sdt);
-    
-    // Getter
-    string getMaSV() const;
-    string getTenSV() const;
-    string getLop() const;
-    string getSDT() const;
-    vector<string> getSachDangMuon() const;
-    
-    // Setter
-    void setTenSV(string ten);
-    void setLop(string l);
-    void setSDT(string sdt);
-    
-    void themSachMuon(string maSach);
-    void xoaSachMuon(string maSach);
-    
-    void nhapThongTin();
-    void hienThi() const;
-    
-    // Đọc/ghi file nhị phân
-    void ghiFile(ofstream &out) const;
-    void docFile(ifstream &in);
-};
+string SinhVien::getMaSV() const { return maSV; }
+string SinhVien::getTenSV() const { return tenSV; }
+string SinhVien::getLop() const { return lop; }
+string SinhVien::getSDT() const { return sdt; }
+vector<string> SinhVien::getSachDangMuon() const { return sachDangMuon; }
 
-#endif
+void SinhVien::setTenSV(string ten) { tenSV = ten; }
+void SinhVien::setLop(string l) { lop = l; }
+void SinhVien::setSDT(string sd) { sdt = sd; }
+
+void SinhVien::themSachMuon(string maSach) {
+    sachDangMuon.push_back(maSach);
+}
+
+void SinhVien::xoaSachMuon(string maSach) {
+    for (auto it = sachDangMuon.begin(); it != sachDangMuon.end(); ++it) {
+        if (*it == maSach) {
+            sachDangMuon.erase(it);
+            break;
+        }
+    }
+}
+
+void SinhVien::nhapThongTin() {
+    cout << "Nhap ma sinh vien: ";
+    getline(cin, maSV);
+    cout << "Nhap ten sinh vien: ";
+    getline(cin, tenSV);
+    cout << "Nhap lop: ";
+    getline(cin, lop);
+    cout << "Nhap so dien thoai: ";
+    getline(cin, sdt);
+}
+
+void SinhVien::hienThi() const {
+    cout << "Ma SV: " << maSV 
+         << " | Ten: " << tenSV 
+         << " | Lop: " << lop 
+         << " | SDT: " << sdt 
+         << " | So sach dang muon: " << sachDangMuon.size() << endl;
+}
+
+void SinhVien::ghiFile(ofstream &out) const {
+    size_t len;
+    
+    len = maSV.size();
+    out.write((char*)&len, sizeof(len));
+    out.write(maSV.c_str(), len);
+    
+    len = tenSV.size();
+    out.write((char*)&len, sizeof(len));
+    out.write(tenSV.c_str(), len);
+    
+    len = lop.size();
+    out.write((char*)&len, sizeof(len));
+    out.write(lop.c_str(), len);
+    
+    len = sdt.size();
+    out.write((char*)&len, sizeof(len));
+    out.write(sdt.c_str(), len);
+    
+    size_t soSach = sachDangMuon.size();
+    out.write((char*)&soSach, sizeof(soSach));
+    for (const auto &ms : sachDangMuon) {
+        len = ms.size();
+        out.write((char*)&len, sizeof(len));
+        out.write(ms.c_str(), len);
+    }
+}
+
+void SinhVien::docFile(ifstream &in) {
+    size_t len;
+    
+    in.read((char*)&len, sizeof(len));
+    maSV.resize(len);
+    in.read(&maSV[0], len);
+    
+    in.read((char*)&len, sizeof(len));
+    tenSV.resize(len);
+    in.read(&tenSV[0], len);
+    
+    in.read((char*)&len, sizeof(len));
+    lop.resize(len);
+    in.read(&lop[0], len);
+    
+    in.read((char*)&len, sizeof(len));
+    sdt.resize(len);
+    in.read(&sdt[0], len);
+    
+    size_t soSach;
+    in.read((char*)&soSach, sizeof(soSach));
+    sachDangMuon.clear();
+    for (size_t i = 0; i < soSach; ++i) {
+        in.read((char*)&len, sizeof(len));
+        string ms;
+        ms.resize(len);
+        in.read(&ms[0], len);
+        sachDangMuon.push_back(ms);
+    }
+}
